5852663597fc1b0c56ebf4cd90a4a8d6e310c844
Jeff Scott Brown <brownj@objectcomputing.com>
2022-04-12 15:45:03 -0500

    https://stackoverflow.com/questions/71848768/how-to-extract-only-a-certain-text-from-a-file

---
+++ app/src/main/groovy/samkraken/App.groovy
@@ -0,0 +1,17 @@
+package samkraken
+
+class App {
+    static void main(String[] args) {
+        def bundle = ResourceBundle.getBundle('versions')
+        println bundle.getString('version.release')
+
+        def version = [:]
+        version.major = bundle.getString('version.major')
+        version.minor = bundle.getString('version.minor')
+        version.maintenance = bundle.getString('version.maintenance')
+        version.build = bundle.getString('version.build')
+
+        def SVN_TAG="ccsmp_v_${version.major}.${version.minor}.${version.maintenance}.${version.build}"
+        println SVN_TAG
+    }
+}
